5bb928355b3d0cc28612657df0249b292692be0b
karla lautero <karla.lautero@estudiantesunibague.edu.co>
2026-02-05 17:54:11 -0500

    first commit

---
+++ src/main/java/org/yourcompany/yourproject/IngenieriaSoftwareDos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Karla Lautero
+ */
+public class IngenieriaSoftwareDos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
